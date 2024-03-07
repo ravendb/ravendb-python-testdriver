eaf6a98986ec0ab0f82b051380cafd83e0da90ce
Gracjan Sadowicz <gracjan@ravendb.net>
2024-03-07 16:07:18 +0100

    Adjust requirements to use 5.2.6 client

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="ravendb-test-driver",
     packages=find_packages(exclude=["*.tests.*", "tests", "*.tests", "tests.*"]),
-    version="6.0.0.post4",
+    version="6.0.0.post5",
     description="RavenDB package for writing integration tests against RavenDB server",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
@@ -13,5 +13,5 @@ setup(
     license="MIT",
     keywords=["ravendb", "nosql", "database", "test", "driver"],
     python_requires="~=3.7",
-    install_requires=["ravendb-embedded~=5.2.5.post1", "ravendb~=5.2.5.post1"],
+    install_requires=["ravendb-embedded~=5.2.6", "ravendb~=5.2.6"],
 )
